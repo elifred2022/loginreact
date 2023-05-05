cbb98e1b02ff11000426f5cfd4be689f8192c830
elifred2022 <elifed21@hotmail.com>
2023-05-05 19:47:23 -0300

    codigo para autenticar y registro de usuario con auth0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+
+import logo from "./Kukenan_Roraima_GS.jpg";
+import "./App.css";
+import { LoginButton } from "./Login";
+import { LogoutButton } from "./Logout";
+import { Profile } from "./Profile";
 
 function App() {
+  const { isLoading, isAuthenticated, loginWithRedirect } = useAuth0();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isAuthenticated ? (
+          <>
+            <Profile />
+
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
       </header>
     </div>
   );
 }
 
 export default App;
+
+// este login lo ree con auth0 = https://manage.auth0.com/dashboard/us/dev-vak1ns36mr6m850i/onboarding
+// user: elifredmason@gmail.com
+// pass: Roraima2003*
+
+// ---- dependencias -----
+// npm install @auth0/auth0-react
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider, Provider } from "@auth0/auth0-react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-vak1ns36mr6m850i.us.auth0.com"
+      clientId="oT9SW2P4UxHoA3eQ7t2Ox46V91oD03XF"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
